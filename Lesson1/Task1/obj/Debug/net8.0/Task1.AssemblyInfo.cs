9eb82f5fde8bffcb7f72126c56f920d30e464d18
Anastasia Mik <mikunova1108@gmail.com>
2024-04-03 14:51:18 +0300

    Added all trash

--- Lesson1/Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
+++ Lesson1/Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5489eabef43e2dc35695438ae3a777afa57c19bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
