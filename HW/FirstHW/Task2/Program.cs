3a1e07320f3ba28e13f4d859bcac92f2c5595441
Anastasia Mik <mikunova1108@gmail.com>
2024-04-08 15:07:32 +0300

    hw task2

---
+++ HW/FirstHW/Task2/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу,  которая  принимает на вход три числа
+// и выдаёт максимальное из этих чисел.
+
+Console.Write("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+int thirdNumber = Convert.ToInt32(Console.ReadLine());
+
+int max = firstNumber;
+
+if (secondNumber > max)
+{
+    max = secondNumber;
+}
+else
+{
+    max = firstNumber;
+}
+
+if (thirdNumber > max)
+{
+    max = thirdNumber;
+}
+else
+{
+    max = max;
+}
+
+Console.WriteLine("Максимальное число равняется: ");
+Console.WriteLine(max);
