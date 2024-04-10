720cdc3f0911780a0ad70ad498ceef3d0f1830a0
Anastasia Mik <mikunova1108@gmail.com>
2024-04-10 16:58:28 +0300

    task 1 hw 2

---
+++ HW/2_HW/Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 1: Напишите программу, которая принимает на вход число и проверяет,
+//кратно ли оно одновременно 7 и 23.
+
+System.Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 7 == 0 && number % 23 == 0)
+{
+    Console.WriteLine("Число кратно 7 и 23");
+}
+else
+{
+    Console.WriteLine("Число не кратно 7 и 23, введите другое число");
+}
