7233b17a9a99a811ec90dae9ea112a70900355a6
Anastasia Mik <mikunova1108@gmail.com>
2024-04-10 17:47:26 +0300

    task2 hw2

---
+++ HW/2_HW/Task2/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход координаты точки (X и Y),
+// причём X ≠ 0 и Y ≠ 0 и выдаёт номер координатной четверти плоскости, в которой находится эта точка.
+// 
+
+System.Console.Write("Введите число x: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("Введите число y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x == 0 || y == 0)
+{
+    Console.WriteLine("Нельзя указывать 0, введите другое число.");
+} 
+else if (x > 0 && y > 0)
+{
+    Console.WriteLine("Вы ввели координаты I четверти.");
+}
+else if (x > 0 && y < 0)
+{
+    Console.WriteLine("Вы ввели координаты II четверти.");
+}
+else if (x < 0 && y < 0)
+{
+    Console.WriteLine("Вы ввели координаты III четверти.");
+}
+else
+{
+    Console.WriteLine("Вы ввели координаты IV четверти.");
+}
+
+Console.WriteLine("Программа завершена.");
