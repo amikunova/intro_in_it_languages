c0dcb3e1bbe84f2f3fb482424432f4789639c6b7
Anastasia Mik <mikunova1108@gmail.com>
2024-04-13 14:26:02 +0300

    Hw 2, task4

---
+++ HW/2_HW/Task4/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая на вход принимает натуральное число N,
+// а на выходе показывает его цифры через запятую
+
+System.Console.Write("Введите натуральное число: ");
+long number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 0)
+{
+    string numberStr = number.ToString();
+    char[] array = numberStr.ToCharArray();
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == array.Length - 1)
+        {
+            Console.Write($"{array[i]}");
+        }
+        else
+        {
+            Console.Write($"{array[i]},");
+        }
+    }
+
+    Console.WriteLine();
+}
+else
+{
+    System.Console.Write("Введите корректное число ");
+}
