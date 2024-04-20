f905e39e0f0d7d7212cb610a1dfbfaf60ebf7e32
Anastasia Mik <mikunova1108@gmail.com>
2024-04-20 23:22:51 +0300

    Add program to count even numbers in an array
    A new program has been added in Task2 that generates an array filled with random three-digit numbers and counts the number of even numbers in it. It includes functions to generate the array, print it, and get the count of even numbers.

---
+++ HW/4_HW/Task2/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задайте массив заполненный случайными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+using System.Threading.Channels;
+
+void Main()
+{
+    int arraySize = ReadInt("Введите размер массива: ");
+    int[] array = GenerateArray(arraySize, 100, 999);
+    PrintArray(array);
+
+    int countOfEven = GetCountOfEvenNumbersFromArray(array);
+
+    Console.WriteLine("Колличество четных чисел в массиве = " + countOfEven);
+}
+
+int GetCountOfEvenNumbersFromArray(int[] array)
+{
+    int count = 0;
+    foreach (int number in array)
+    {
+        if (number % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+void PrintArray(int[] arrayForPrint)
+{
+    for(int i = 0; i < arrayForPrint.Length; i++)
+        System.Console.Write(arrayForPrint[i] + " ");
+    System.Console.WriteLine();
+}
+
+int[] GenerateArray(int size, int minRange, int maxRange)
+{
+    int[] tempArray = new int[size];
+    Random rand = new Random();
+
+    for (int i = 0; i < size; i++)
+        tempArray[i] = rand.Next(minRange, maxRange);
+
+    return tempArray;
+}
+
+int ReadInt(string msg)
+{
+    System.Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+Main();
