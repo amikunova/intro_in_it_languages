ad300e59c2ff5d4cc697957d4f20c0a2592c56be
Anastasia Mik <mikunova1108@gmail.com>
2024-04-20 16:49:14 +0300

    lesson 3, task2

---
+++ Lessons/Lesson3/Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Concurrent;
+
+void PrintSquares(int limit)
+{
+    int i = 1;
+    while (i <= limit)
+    {
+        Console.Write($"{i*i}");
+        i++;
+    }
+}
+PrintSquares(5);
+Console.WriteLine();
+
+PrintSquares(10);
+Console.WriteLine();
+
+PrintSquares(15);
