124c375b28e48acddf2706bb79ee69f1956bd21d
Anastasia Mik <mikunova1108@gmail.com>
2024-04-20 22:54:05 +0300

    Add interactive integer input Program
    This commit introduces a new Program.cs that endlessly polls for integers from the console till the sum of the input digits is even or the user enters 'q'. It includes error handling for non-digit inputs, providing a more robust and user-friendly experience.

---
+++ HW/4_HW/Task1/Program.cs
@@ -0,0 +1,40 @@
+﻿// Напишите программу, которая бесконечно запрашивает целые числа с консоли.
+// Программа завершается при вводе символа ‘q’ или при вводе числа, сумма цифр которого чётная.
+
+
+while (true)
+{
+    Console.WriteLine("Введите число:");
+    string input = Console.ReadLine();
+    if (input == "q" || input == "Q")
+    {
+        Console.WriteLine("Вы ввели 'q', программа завершена");
+        return;
+    }
+
+    int sum = 0;
+
+    foreach (char digit in input)
+    {
+        if (!char.IsDigit(digit))
+        {
+            Console.WriteLine("Ошибка: введенное значение не является цифрой");
+            sum = 0;
+            break;
+        }
+        
+        sum = int.Parse(digit.ToString()) + sum;
+    }
+
+    if (sum == 0)
+    {
+        continue;
+    }
+    
+    if (sum % 2 == 0)
+    {
+        Console.WriteLine("Сумма цифр числа чётная, программа завершена");
+        return;
+    }
+    
+}
