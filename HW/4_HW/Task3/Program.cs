cef07021be21568305a11cae58d9760be8cb2323
Anastasia Mik <mikunova1108@gmail.com>
2024-04-21 10:01:08 +0300

    Add Task3 to Homework 4
    The commit introduces Task3 in Homework 4, which generates an array, prints it, reverses the elements, and then prints the reversed array. It includes functions to create an array, read an integer input, reverse the array, and print the array.

---
+++ HW/4_HW/Task3/Program.cs
@@ -0,0 +1,46 @@
+﻿
+void Main()
+{
+    int arraySize = ReadInt("Введите размер массива: ");
+    int[] array = GenerateArray(arraySize, 0, 9);
+    PrintArray(array);
+    RevertArray(array);
+    PrintArray(array);
+}
+
+void RevertArray(int[] array)
+{
+
+    for (int i = 0; i < array.Length / 2; i++)
+    {
+        int temp = array[i];
+        array[i] = array[array.Length - 1 - i];
+        array[array.Length - 1 - i] = temp;
+    }
+}
+
+void PrintArray(int[] arrayForPrint)
+{
+    for(int i = 0; i < arrayForPrint.Length; i++)
+        System.Console.Write(arrayForPrint[i] + " ");
+    System.Console.WriteLine();
+}
+
+int[] GenerateArray(int size, int minRange, int maxRange)
+{
+    int[] tempArray = new int[size];
+    Random rand = new Random();
+
+    for (int i = 0; i < size; i++)
+        tempArray[i] = rand.Next(minRange, maxRange);
+
+    return tempArray;
+}
+
+int ReadInt(string msg)
+{
+    System.Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+Main();
