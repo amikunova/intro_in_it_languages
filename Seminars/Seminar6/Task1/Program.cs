cf7a76c707e60a856673e0135e17fb51aa18c80e
Anastasia Mik <mikunova1108@gmail.com>
2024-04-21 10:49:56 +0300

    Seminar6, tasks

---
+++ Seminars/Seminar6/Task1/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задайте массив символов (тип char []). Создайте строку из
+// символов этого массива.
+// Указание
+// Конструктор строки вида string(char []) не использовать.
+// Пример
+// [‘a’, ‘b’, ‘c’, ‘d’] => “abcd”
+
+void Main()
+{
+    char[] chars = { 'a', 'b', 'c', 'd' };
+
+    // 1-й вариант:
+    // string str = new string(chars);
+
+    // 2-й вариант:
+    // string str = ArrayToString(chars);
+
+    // 3-й вариант:
+    // string str = string.Concat(chars);
+
+    // 4-й вариант:
+    string str = string.Join("", chars);
+    System.Console.WriteLine(str);
+}
+
+string ArrayToString(char[] chars)
+{
+    string str = "";
+
+    foreach (char letter in chars)
+        str += letter;
+
+    return str;
+}
+
+Main();
---
+++ Seminars/Seminar6/Task2/Program.cs
@@ -0,0 +1,36 @@
+﻿// На основе символов строки (тип string) сформировать массив
+// символов (тип char[]). Вывести массив на экран.
+// Указание
+// Метод строки ToCharArray() не использовать.
+// Пример
+// “Hello!” => [‘H’, ‘e’, ‘l’, ‘l’, ‘o’, ‘!’ ] 
+
+void Main()
+{
+    System.Console.Write("Введите текст: ");
+    string? str = Console.ReadLine(); 
+
+    // char[] chars = str!.ToCharArray();
+    // PrintArray(chars);
+
+    char[] chars = StringToCharArray(str);
+    PrintArray(chars);
+}
+
+char[] StringToCharArray(string? str)
+{
+    char[] tempChars = new char[str!.Length];
+
+    for(int i = 0; i < str.Length; i++)
+        tempChars[i] = str[i];
+
+    return tempChars;
+}
+
+
+void PrintArray(char[] chars)
+{
+    System.Console.WriteLine("[" + string.Join(", ", chars) + " ]");
+}
+
+Main();
