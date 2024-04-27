59b71d5471d6371255ce050184fa1affde7c01f4
Anastasia Mik <mikunova1108@gmail.com>
2024-04-27 22:49:41 +0300

    дз 6

---
+++ HW/6_HW/Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿//  Задайте двумерный массив символов (тип char [,]).
+// Создать строку из символов этого массива.
+
+
+char[,] charArray = { { '1', '2', '3' }, { '4', '5', '6' } };
+
+// string str = new string(charArray.Cast<char>().ToArray());
+//
+// Console.WriteLine(str);
+
+string str = "";
+
+foreach (char letter in charArray)
+{
+    str = str + letter;
+}
+Console.WriteLine(str);
---
+++ HW/6_HW/Task3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте произвольную строку. Выясните, является ли она палиндромом.
+
+string str = "шалаш";
+
+bool isPalindrom(string str)
+{
+    string revString = new string(str.Reverse().ToArray());
+    return str == revString;
+}
+
+if (isPalindrom(str))
+{
+    Console.WriteLine("Строка палиндром");
+}
+else
+{
+    Console.WriteLine("Строка не является палиндромом");
+}
