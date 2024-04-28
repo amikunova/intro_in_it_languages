e7d53725d06b02ec07268b8f0d3747c0fe446c78
Anastasia Mik <mikunova1108@gmail.com>
2024-04-28 16:08:39 +0300

    hw 3

---
+++ HW/3_HW/Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задайте одномерный массив из 10 целых чисел от 1 до 100.
+// Найдите количество элементов массива, значения которых лежат в отрезке [10,90].
+// 
+
+int[] array = new int[10];
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+  array[i] = new Random().Next(1, 100);
+  System.Console.Write(array[i] + " ");
+  
+  if (array[i] >= 10 && array[i] <= 90)
+  {
+    count++;
+  }
+}
+
+Console.WriteLine();
+System.Console.Write($"количество элементов массива, значения которых лежат в отрезке [10,90] = {count}");
+Console.WriteLine();
+
+
---
+++ HW/3_HW/Task3/Program.cs
@@ -0,0 +1,28 @@
+﻿// 
+
+double[] array = {0.25, 5.4, 1.3, 2.1, 3.8, 5.2, 3.01};
+double max = array[0];
+double min = array[0];
+
+for (int i = 1; i < array.Length; i++)
+{
+    System.Console.Write(array[i] + " ");
+
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+
+    if (array[i] < min)
+    {
+        min = array[i];
+    }
+}
+
+System.Console.WriteLine();
+System.Console.WriteLine("Max: " + max);
+System.Console.WriteLine("Min: " + min);
+
+double result = max - min;
+System.Console.WriteLine();
+System.Console.WriteLine("result: " + result);
